afd641df5b1669e28267b46c889878dadc6b70dd
Damian <oti@Brandons-MacBook-Pro.local>
2015-02-06 18:21:22 -0800

    Seeing if drupal is possible, need more angular, focus...

---
+++ src/main/js/app.js
@@ -0,0 +1,8 @@
+
+
+angular.module('xanon').constant('MONGOLAB_CONFIG', {   //need to configure the MONGOLAB_CONFIG var to meet my database
+  baseUrl: '/databases/',
+  dbName: 'ascrum'
+});
+
+angular.module('xanon').configure('LPCntlr'), {, }  //variables need to be configured in 

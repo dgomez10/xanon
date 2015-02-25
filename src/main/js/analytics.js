94d2206787673b582edae31de51628e5630308d9
dgomez10 <dgomez1@scu.edu>
2015-02-25 11:42:30 -0800

    Further Segment; Need to Load Library

--- src/main/js/analytics.js
+++ src/main/js/analytics.js
@@ -15,7 +15,35 @@
 
 
 //Analytics Methods: Page 
+analytics.page('Views' {
 
+  "anonymousId": "507f191e810c19729de860ea",
+  "channel": "browser",
+  "context": {
+    "ip": "8.8.8.8",
+    "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36"
+  },
+  "event": "Registered",
+  "integrations": {
+    "All": true,
+    "Mixpanel": false,
+    "Salesforce": false
+    "Google Analytics": true
+  },
+  "messageId": "022bb90c-bbac-11e4-8dfc-aa07a5b093db",  //auto-generate anon? 
+  "name": "Home",
+  "projectId": "sjdhv6wim",
+  "properties": {
+    "title": "Xanon",
+    "url": "http://www.xanon.us"
+    "path": "url/[blog], [about], [signin]" // how to specify the subdomain?
+  },
+  "receivedAt": "2015-02-23T22:28:55.387Z",
+  "sentAt": "2015-02-23T22:28:55.111Z",
+  "timestamp": "2015-02-23T22:28:55.111Z",
+  "type": "page",
+  "version": "1.1"
+});
 
 // Identify User -- Create [anonymousId] token? 
 analytics.identify('1e810c197e', {
@@ -23,7 +51,7 @@ analytics.identify('1e810c197e', {
   email: 'bill@initech.com'
 });
 
-//This to track 
+//track Clicks 
 analytics.trackClick([userID], {
 	//possible to id word
 	//# clicks total
@@ -36,3 +64,13 @@ analytics.trackClick([userID], {
     'Google Analytics': false
   }
 });
+
+
+//track Links
+var link = document.getElementById('free-trial-link');
+
+analytics.trackLink(link, 'Clicked Free-Trial Link', {
+  plan: 'Enterprise'
+});
+
+

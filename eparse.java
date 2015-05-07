a1f1fd2605511868906c7a92187f00f7eea2dcfc
dgomez10 <dgomez1@scu.edu>
2015-05-07 10:25:07 -0700

    Matz harder implement

---
+++ eparse.java
@@ -0,0 +1,59 @@
+import com.psql.jdbc.Driver;
+import java.io;
+import java.util;
+import org.apache.HTTP.*; 
+import android.os.Parcelable.Creator.*; 
+import org.postgresql.Connection; 
+import java.math
+
+
+public static Eparse{
+  public static main(String[] args){
+	//import emotions
+	 .statements(EID); 
+	//import lexical
+	.statements("lexcical")
+	parse.statements("positive");
+	parse.toHex();
+	Parse.ByteArrayOutStream.statements("negative"); 
+	parse.toHex();		
+	
+
+}
+
+ public static fuzzyTruth(String[] args) {
+	Math sigmoid(a)= 1/(1+E^-x)
+	sigmoid(a) = [-1, 1]
+	if (0 < sigmoid < 1){
+		max = sigmoid.results.statements();
+		//tie into blockchain and opsmsg
+	}
+	else (-1 < sigmoid <= 0){
+		min = sigmoid.results(null);
+		//sent to eval system
+	}
+	Imply = ModF limit(x, y) = min{1, 1-x+y}
+	Equal = F = (x, y) = 1 - |x+y|
+	Negate = F set of(x) = 1 - x ;
+	Weak mamdani intersect = F ^ (x, y) = min{x, y}
+	Weak disjunction = max{x,y}
+	Strong mamdani intersect = max{0, x+y-1}
+	Strong disjunction = min{1, x+y}
+}
+
+ public static kLoeve{
+	//describe Module
+	ModF(x) = {(x1, t1, w1)...xn};
+	ModC(z)	= {(z1, t1, w1),...zn}; 
+	   
+	cos{pi/4( zuc - u/z)} / [ tan pi/ 4[(u)  +dNz + dT + dw]] 
+
+	//lexical travel 
+{T ⇒ transposed}: 
+		[uz^T X Nu(z-1)] - wavelength distance LAMBDA[ uz-1^(T) *u(z-1)- 1] - phi {//rate}u(z-1)^T * N(z-1) ] 
+	//emotional valence
+phi [rate] x(t-1) =  x - SUM (ux^T ,  ux , x )  
+	//likely to speak 
+	ut = arg MAX E[(u^T ˆx(t-1)^2)]		
+
+}

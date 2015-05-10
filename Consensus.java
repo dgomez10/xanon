429409f0a394a064e7c8fa357430badaa231243f
dgomez10 <dgomez1@scu.edu>
2015-05-09 22:46:04 -0700

    Consensus

--- Consensus.java
+++ Consensus.java
@@ -10,53 +10,59 @@ import java.lang.Object;
 
 
 public static Consensus{
-  public static main(String[] args){
-	leader.onStart(); 
+  public static main(String[] args extends Thread, LeaderSelector){
+	integrity.start.setId.getLeader("vocalist"); 
 	evilDigestors.getTotal();	
-	EventListener regentPropose = HashMap<>('capacity') << BufferedOuputStream(FileReader("Intergrity.xml")) //represents array of proposals range?  
-	 var recuse = regentPropse<>('Integrity', "capacity" ,'Hash#').generate('PC')
-		Array<>PC = recuse + [(digestors - fault) >> check; if check < 'recuse' digestor.release(); 
-				else check >= 'recuse' digestor.accept(); else if check == null digestor.<revote>()
-		leader := List<>("PC", "recuse","key", "message", "DH"); 
+	LeaderSelectorListener regentPropose.requeue = HashMap<>('capacity') << BufferedOuputStream(FileReader("Intergrity.xml"))
+	BufferedInputStream(FileInputStream(pg_relation_path.listen("recuse.log")))
+	//PC
+	 var recuse = regentPropose<>("trust", "capacity" ,'Hash#').generate("PC"): HashMap.getKey()  //progcert use-case?
+	 var check = (digestors - fault);
+	 if (recuse <= check);  
+		{recuse.release();} 
+	 else if "check" >= "recuse" 
+		{recuse.hasLeadership();}
+	 else (check || recuse = null) 
+		{digestor.requeue();}
+
+	proposedLeader := OtpErlangMap<? extends "recuse">RecuseMap("key", "message", "DH"); 
 
 	
 }
 
- public static Diffusion(String[], args extends Thread){
-	OtpErlangMap<>RecuseMap(OtpErlangObject[int recuse])
-	 BufferedInputStream(FileInputStream(pg_relation_path.listen("recuse.log")))
-	 Oldrecuse := leader[recuse];  //pending database structure
-	if (leader["PC"] != null)
+ public static brainDiffusion(String[], args extends Thread){
+	firstBrain = LeaderSelector.getId("proposedLeader");
+	Oldrecuse := firstBrain.hasLeadership();  //pending database structure
+	OtpErlangObject fault = (RecuseMap.getByteArrayOutPut() + integrity.fault.getId() + 1)/2
+	voteMessage := Array<>(proof = FileReader(FileInputStream.(("proof.log"), 'S',"proposedLeader")));
+	if (|proposedLeader| > fault)
 	{
-	voteMessage := Array<>{(proof = FileInputStream.(("*.log"),"leader"));}
-	voteMessage.getRelease("recuse");}
-	|S| >= [(p+f+1)/2] for S === [peopleEmotion.getTotal('EID') + electionVote.getHashCode.getBool(true)] < evilDigestors.getTotal();   
+		recuse.push.voteMessage.send('S');
+	}
+	else 
+	voteMessage.requeue.getRelease();  //leader-election.suspect(leader - election.getRegency computations adds validity - how?)
+	|S| >= [(p+f+1)/2] for S == [peopleEmotion.getTotal('EID') + electionVote.getHashCode.getBool(true)] < evilDigestors.getTotal(); 
+	for brainers.notifyAll.getId('S').getParticipants();{ 
+	  LeaderSelector(brainers.onPropose("recuse", "Integrity"): "proposedLeader"); //learning accept 1st node???
+	 	if (true == brainers.getParticipants()){firstBrain <= (brainers.notify("regentPropose") + 3(fault) + 1)/2 ;}
+		
+		brainers.notifyAll.voterMessage.accept.getLeader("proposedLeader");} //securityIssue?
 }
 
 
- public static Learn(String[], args extends Thread){
-	brainers[0] = diffusion.serialize("voteMessage"){
-		electionResult = digestors + fault + 1)/2 <= |S|
-		local.isDaemon() = Thread(//server connection)
-		if(electionResult == true);{
-		brainers.notifyAll = leader.countDown.getIdentifier("recuse"){Thread brainers.local.add('PC') }
-			{
-		for (HashMap.Entry<"leader"> entry : map.entrySet(peopleIntegrity.getKey('KID'))) //time specific
-			 {
-			diffusion.putAll.getRelease('S') >> Set<? extends RecuseMap>brainers.doHashCode();
-			}
-		diffusion.send('S') //to learners// 
-		else  		
-}
+ public static Learn(String[], args extends Thread){//consensus that nodes should recieve 'king' node
+	learners[0] = brainDiffusion.serialize("proposedLeader"){
+		local.isDaemon() = Thread(//server connection) //check api c psql... 
+		regentPropse.learners.notifyAll.wait(int 400); learners.pull[0]{for proposedLeader.getKey("trust", brainers.accept(true))
+				learners.notify.release()}
+		learners.doHashCode() >> HashMap.Entry<"leader"> entry : map.entrySet(peopleIntegrity.getKey('KID')
+		learners.notifyAll.accept("entry")
 
-}
-
- public static electionVote{
-  	OptMessageObject learnNode = learners.notifyAll.recieveMsg('S'); 
-		learnNode.serial.decode("recuse" "PC");
-	if pnumeber >= oldRecuse{ evaluation.sendQuery() }; 
-	evalulation = 
-	void leader = if election.suspect(int 'Integrity'){peaceValue <= (digestors + fault + 1)/2  }
+	  	OptMessageObject electionVote = learners.notifyAll.recieveMsg('S'); 
+		electionVote.serial.decode("regentPropose");
+		do regentPropose.getId("trust") >= oldRecuse{ evaluation.getId("trust"); 
+			evalulation = fault + 1 + learners.getParticipants();}
+		void leader = if electionVote.getId("trust"){peaceValue <= (digestors + fault + 1)/2 } //but not opp
 }
 
 

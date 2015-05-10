6dd53bd3714550ad6fee2edb76dc07a0b7b1c206
dgomez10 <dgomez1@scu.edu>
2015-05-09 20:37:24 -0700

    kinks

--- Consensus.java
+++ Consensus.java
@@ -1,10 +1,10 @@
-
 import java.util.*; 
 import com.psql.jdbc.Driver;
 import java.io;
 import org.apache.HTTP.*;
 import org.postgresql.Connection; 
 import java.math;
+import java.lang.Object; 
 
 
 
@@ -22,33 +22,49 @@ public static Consensus{
 	
 }
 
- public static Diffusion{
-	oldRecuse := leader[recuse];
+ public static Diffusion(String[], args extends Thread){
+	OtpErlangMap<>RecuseMap(OtpErlangObject[int recuse])
+	 BufferedInputStream(FileInputStream(pg_relation_path.listen("recuse.log")))
+	 Oldrecuse := leader[recuse];  //pending database structure
 	if (leader["PC"] != null)
 	{
-	voteMessage := Array<>{(proof = FileInputStream.(("*.log"),"leader", "leader[recuse]");}
+	voteMessage := Array<>{(proof = FileInputStream.(("*.log"),"leader"));}
 	voteMessage.getRelease("recuse");}
 	|S| >= [(p+f+1)/2] for S === [peopleEmotion.getTotal('EID') + electionVote.getHashCode.getBool(true)] < evilDigestors.getTotal();   
 }
 
 
- public static Learn{
-	brainers[0] = diffusion.onLearned("voteMessage"){
-		for{(digestors + fault + 1)/2 <= |S|;}
-		  diffusion.getRelease('S') >> brainers['i'].accept();
-	brainers[0] == (.accept()){}
-	pnumber = Integrity.getHashCode("KID")/Integrity.getHashCode("peopleIntegrity"); 
-	
+ public static Learn(String[], args extends Thread){
+	brainers[0] = diffusion.serialize("voteMessage"){
+		electionResult = digestors + fault + 1)/2 <= |S|
+		local.isDaemon() = Thread(//server connection)
+		if(electionResult == true);{
+		brainers.notifyAll = leader.countDown.getIdentifier("recuse"){Thread brainers.local.add('PC') }
+			{
+		for (HashMap.Entry<"leader"> entry : map.entrySet(peopleIntegrity.getKey('KID'))) //time specific
+			 {
+			diffusion.putAll.getRelease('S') >> Set<? extends RecuseMap>brainers.doHashCode();
+			}
+		diffusion.send('S') //to learners// 
+		else  		
+}
 
 }
 
  public static electionVote{
-	brainers.onQuery("vote");  
+  	OptMessageObject learnNode = learners.notifyAll.recieveMsg('S'); 
+		learnNode.serial.decode("recuse" "PC");
 	if pnumeber >= oldRecuse{ evaluation.sendQuery() }; 
 	evalulation = 
 	void leader = if election.suspect(int 'Integrity'){peaceValue <= (digestors + fault + 1)/2  }
 }
 
+
+ public static resolveEvil{
+	 OtpInputStream(pnumber) = integrity.getHashCode("KID")/integrity.getHashCode.getKey("peopleIntegrity");
+	
+
+}
 
 
 

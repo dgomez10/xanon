80b1359bfb25ca73cf42f20917d69b64dcd5cd34
dgomez10 <dgomez1@scu.edu>
2015-05-11 21:12:28 -0700

    proj

---
+++ Qcode.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+import com.psql.jdbc.Driver;
+import java.io;
+import org.apache.HTTP.*;
+import org.postgresql.Connection;
+import java.math;
+import java.lang.Object;
+
+import android.view.SurfaceHolder;
+import android.view.*;
+
+
+public class Qcode{
+    public static main(Sting [] args){
+            .onGenericMotionListener(View scanScreen, focusCamera, scanBarCode, Event openApp);
+        View openApp = scanScreen.getSurface(450, 600);
+        Canvas scanScreen =  openApp.lockCanvas(Rect clean);
+    }
+
+    public static focusCamera(String[], args){
+        focusCamera.onTouch();
+        { (CameraManager) getSystemService(Context.CAMERA_SERVICE).addCallback(appMemCache, mint);
+            @JavaScriptInterface
+            appMemCache = scanScreen.openConnection("https//:www.api.com");
+            mint = MemoryFile(addJavaScriptInterface.getInputStream.openStream(appMemCache));}
+
+    }
+    public static scanBarCode(){
+        scanBarCode.onTouch();
+        BarCodeSerializer getBase = getDevice.capture("scanBarCode")
+        if (pictureTaken == true) {.getData() = CameraBarCode getBarCode = scanBarCode.getCallback(appMemCahche);
+            Parcelable.writeToParcel(dest, PARCEABLE_WRITE_RETURN_VALUE);
+            SendBarCode portRequestSend = getBase.createBinderArray(int LIKE_TRANSACTION);
+        }
+    }
+}
+}
